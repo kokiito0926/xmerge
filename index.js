8bf51c2a470254f350d5dfe8c87fb07d6b9ef8ff
Koki Ito <kokiito0926@gmail.com>
2026-02-09 15:29:46 +0900

    feat: 🎸 複数のファイルに対応した

--- index.js
+++ index.js
@@ -3,30 +3,55 @@
 // >> $ ./index.js ./aaaa.xml ./bbbb.xml > ./cccc.xml
 
 import { fs, path, stdin, argv } from "zx";
-import { parseStringPromise, Builder } from "xml2js";
+import xml2js from "xml2js";
 import _ from "lodash";
 
-if (argv._.length < 2) {
+async function isValidXML(str) {
+	try {
+		await xml2js.parseStringPromise(str);
+		return true;
+	} catch (err) {
+		return false;
+	}
+}
+
+const files = argv._;
+if (files.length < 2) {
 	process.exit(1);
 }
 
-const [basePath, patchPath] = argv._;
-
-const baseRaw = await fs.readFile(basePath, "utf-8");
-const patchRaw = await fs.readFile(patchPath, "utf-8");
-
-const baseObj = await parseStringPromise(baseRaw);
-const patchObj = await parseStringPromise(patchRaw);
-
-const customizer = (objValue, srcValue) => {
-	if (_.isArray(objValue)) {
-		return objValue.concat(srcValue);
+let resultObj = {};
+for (let i = 0; i < files.length; i++) {
+	const file = files[i];
+	if (!(await fs.pathExists(file))) {
+		continue;
 	}
-};
 
-const mergedObj = _.mergeWith({}, baseObj, patchObj, customizer);
+	const content = await fs.readFile(file, "utf-8");
+	if (!content) {
+		continue;
+	}
 
-const builder = new Builder();
-const xml = builder.buildObject(mergedObj);
+	if (!(await isValidXML(content))) {
+		continue;
+	}
 
+	const currentObj = await xml2js.parseStringPromise(content);
+	if (!currentObj) {
+		continue;
+	}
+
+	if (i === 0) {
+		resultObj = currentObj;
+	} else {
+		resultObj = _.mergeWith(resultObj, currentObj, (objValue, srcValue) => {
+			if (_.isArray(objValue)) {
+				return objValue.concat(srcValue);
+			}
+		});
+	}
+}
+
+const builder = new xml2js.Builder();
+const xml = builder.buildObject(resultObj);
 console.log(xml);
